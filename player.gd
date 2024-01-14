67175afb8ce07dea5f7c3de83958154f5464661c
datashaman <marlinf@datashaman.com>
2024-01-14 13:49:39 +0200

    refactor

--- player.gd
+++ player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
-@export var proximity = 500
+@export var proximity = 300
 @export var margin = 100
-@export var speed = 500
+@export var speed = 600
 
 @export var bullet_scene : PackedScene
+
+var mouse_pos
 
 func _init():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -18,20 +20,19 @@ func _input(event):
 	if event.is_action_pressed("shoot"):
 		shoot()
 
-func get_input():
-	var mouse_pos = get_global_mouse_position()
-	look_at(mouse_pos)
-	
+func _physics_process(_delta):
+	mouse_pos = get_global_mouse_position()
+
 	var dir = Input.get_axis("move_down", "move_up")
 	var dist = position.distance_to(mouse_pos)
-	
+
 	velocity = transform.x * dir * speed
-	
+
 	if Input.is_action_pressed("move_up"):
 		velocity *= smoothstep(0, proximity, dist - margin)
-		
-	$Crosshairs.set_global_position(mouse_pos)
 
-func _physics_process(_delta):
-	get_input()
 	move_and_slide()
+
+func _process(_delta):
+	look_at(mouse_pos)
+	$Crosshairs.set_global_position(mouse_pos)
